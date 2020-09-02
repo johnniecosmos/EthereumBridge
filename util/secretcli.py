170d28dca7dc854e96eb4899315e14c4e4446191
Guy <tbk.guy@gmail.com>
2020-09-02 16:22:58 +0300

    use global secretcli

--- util/secretcli.py
+++ util/secretcli.py
@@ -14,7 +14,7 @@ else:
 def sign_tx(unsigned_tx: str, multi_sig_account_addr: str, account_name: str):
     secret_cli_posix = Path(secret_cli).as_posix()
     unsigned_tx_posix = Path(unsigned_tx).as_posix()
-    cmd = [secret_cli_posix, 'tx', 'sign', unsigned_tx_posix, '--signature-only', '--multisig',multi_sig_account_addr,
+    cmd = ['SGX_MODE=SW', 'secretcli', 'tx', 'sign', unsigned_tx_posix, '--signature-only', '--multisig',multi_sig_account_addr,
            '--from', account_name]
     return run_secret_cli(cmd)
     # return run_secret_cli(" ".join(cmd))
@@ -23,7 +23,7 @@ def sign_tx(unsigned_tx: str, multi_sig_account_addr: str, account_name: str):
 def multisign_tx(unsigned_tx_path: str, multi_sig_account_name: str, *signed_tx):
     secret_cli_posix = Path(secret_cli).as_posix()
     signed_tx_posix = map(lambda x: Path(x).as_posix(), signed_tx)
-    return run_secret_cli([secret_cli_posix, 'tx', 'multisign', unsigned_tx_path, multi_sig_account_name]+
+    return run_secret_cli(['secretcli', 'tx', 'multisign', unsigned_tx_path, multi_sig_account_name]+
                           list(signed_tx_posix))
     # return run_secret_cli(" ".join([secret_cli_posix, 'tx', 'multisign', unsigned_tx_path, multi_sig_account_name]
     #                                + list(signed_tx_posix)))
@@ -33,7 +33,7 @@ def create_unsigined_tx(secret_contract_addr: str, encoded_args: str, multisig_a
     secret_cli_posix = Path(secret_cli).as_posix()
     enclave_key_posix = Path(enclave_key).as_posix()
 
-    cmd = [secret_cli_posix, 'tx', 'compute', 'execute', secret_contract_addr, f"'{encoded_args}'",
+    cmd = ['secretcli', 'tx', 'compute', 'execute', secret_contract_addr, f"'{encoded_args}'",
            '--generate-only', '--chain-id', f"{chain_id}", '--enclave-key', enclave_key_posix, '--code-hash',
            enclave_hash, '--from', multisig_acc_addr]
     return run_secret_cli(cmd)
@@ -43,14 +43,14 @@ def create_unsigined_tx(secret_contract_addr: str, encoded_args: str, multisig_a
 def broadcast(signed_tx_path: str):
     secret_cli_posix = Path(secret_cli).as_posix()
     signed_tx_path_posix = Path(signed_tx_path).as_posix()
-    cmd = [secret_cli_posix, 'tx', 'broadcast', signed_tx_path_posix]
+    cmd = ['secretcli', 'tx', 'broadcast', signed_tx_path_posix]
     return run_secret_cli(cmd)
     # return run_secret_cli(" ".join(cmd))
 
 
 def decrypt(data):
     secret_cli_posix = Path(secret_cli).as_posix()
-    cmd = [secret_cli_posix, 'query', 'compute', 'decrypt', data]
+    cmd = ['secretcli', 'query', 'compute', 'decrypt', data]
     return run_secret_cli(cmd)
     # return run_secret_cli(" ".join(cmd))
 
