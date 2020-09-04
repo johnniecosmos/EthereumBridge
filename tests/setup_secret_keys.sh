57ed393d3721b429d3de462fa7d892840b46a9a9
Guy <tbk.guy@gmail.com>
2020-09-04 18:01:35 +0300

    update keys setup

--- tests/setup_secret_keys.sh
+++ tests/setup_secret_keys.sh
@@ -1,18 +1,26 @@
-# args:
-# $1 = number of signing accounts
-export SGX_MODE=SW
+if (("$#" != 2))
+then
+  echo "Usage: <threshold> <base_dir>"
+  exit 1
+fi
 
-# Setup secretcli with configuration for unit-testing
+threshold=$1
+keys_directory="$2/keys"
+deployment_directory="$2/deployment"
+
+mkdir -p "$keys_directory"
+mkdir -p "$deployment_directory"
+
+# Setup secretcli configuration
 secretcli config chain-id enigma-testnet
-#secretcli config node tcp://127.0.0.1:26657
 secretcli config output json
 secretcli config indent true
 secretcli config trust-node true
-secretcli query register secret-network-params
 
-keys_directory="$(dirname "${BASH_SOURCE[0]}")/keys"
-# Make keys directory if doesn't exist
-mkdir -p "$keys_directory"
+# query register creates cert files in the path of execution
+(cd "$deployment_directory" && secretcli query register secret-network-params)
+
+
 
 # Create accounts if they do not exist and save output to $keys_directory
 for (( i=1; i <= $1; i++ ))
@@ -25,18 +33,18 @@ done
 accounts=${accounts::-1}
 
 # Create multisig account if it doesn't exist
-if [ ! -e  "$keys_directory/ms$1.json" ]; then
-    secretcli keys add "--multisig=$accounts" "--multisig-threshold=$1" "ms$1" &> "$keys_directory/ms$1.json"
+if [ ! -e  "$keys_directory/ms$threshold.json" ]; then
+    secretcli keys add "--multisig=$accounts" "--multisig-threshold=$threshold" "ms$threshold" &> "$keys_directory/ms$threshold.json"
 fi
 
 # Send money to signing accounts
 moneyAddr=$(docker exec secretdev secretcli keys show a -a)
-for (( i=1; i <= $1; i++ ))
+for (( i=1; i <= threshold; i++ ))
 do
   signerAddr=$(secretcli keys show "t$i" -a)
   docker exec -it secretdev secretcli tx send -y "$moneyAddr" "$signerAddr" 10000000uscrt -b block
 done
 
 # Send money to multisig account
-multisigAddr=$(secretcli keys show "ms$1" -a)
+multisigAddr=$(secretcli keys show "ms$threshold" -a)
 docker exec -it secretdev secretcli tx send -y "$moneyAddr" "$multisigAddr" 10000000uscrt -b block
