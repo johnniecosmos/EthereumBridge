f510b079d7abf5626a5d4a88f460e6b9b9061902
Guy <tbk.guy@gmail.com>
2020-09-08 20:24:37 +0300

    scrt->ethr signing logic initial

--- src/signer.py
+++ src/signer.py
@@ -1,5 +1,6 @@
 import json
 from collections import namedtuple
+from threading import Thread, Lock
 from typing import Dict
 
 from mongoengine import signals
@@ -9,6 +10,7 @@ from src.contracts.contract import Contract
 from src.db.collections.eth_swap import ETHSwap, Status
 from src.db.collections.log import Logs
 from src.db.collections.signatures import Signatures
+from src.event_listener import EventListener
 from src.util.common import temp_file
 from src.util.exceptions import catch_and_log
 from src.util.logger import get_logger
@@ -21,16 +23,22 @@ MultiSig = namedtuple('MultiSig', ['multisig_acc_addr', 'signer_acc_name'])
 class Signer:
     """Verifies Ethereum tx in SWAP_STATUS_UNSIGNED and adds it's signature"""
 
-    def __init__(self, provider: Web3, multisig_: MultiSig, contract: Contract, config):
+    def __init__(self, event_listener: EventListener, provider: Web3, multisig_: MultiSig, contract: Contract, config):
         self.provider = provider
         self.multisig = multisig_
         self.contract = contract
+        self.logger = get_logger(db_name=config.db_name, logger_name=config.db_name)
+        self.processed_submission_tx = set()
+
+        self.submissions_lock = Lock()
 
         signals.post_save.connect(self._new_tx_signal, sender=ETHSwap)
-        self.logger = get_logger(db_name=config.db_name, logger_name=config.db_name)
-        self.catch_up()
+        event_listener.register(self.handle_submission, ['Submission'])
+        Thread(target=self._swap_catch_up).start()
+        Thread(target=self._submission_catch_up).start()
 
-    def catch_up(self):
+    def _swap_catch_up(self):
+        """Scans the db for unsigned swap tx and signs them"""
         for tx in ETHSwap.objects(status=Status.SWAP_STATUS_UNSIGNED.value):
             try:
                 self._sign_tx(tx)
@@ -39,6 +47,7 @@ class Signer:
 
     # noinspection PyUnusedLocal
     def _new_tx_signal(self, sender, document, **kwargs):
+        """Callback function to handle db signals"""
         if not document.status == Status.SWAP_STATUS_UNSIGNED.value:
             return
         try:
@@ -47,11 +56,12 @@ class Signer:
             self.logger.error(msg=e)
 
     def _sign_tx(self, tx: ETHSwap):
-        if self.is_signed(tx):
+        """Makes sure that the tx is valid and signs it"""
+        if self._is_swap_signed(tx):
             Logs(log=f"Tried to sign an already signed tx. Signer:\n {self.multisig.signer_acc_name}.\ntx id:{tx.id}.")
             return
 
-        if not self.is_valid(tx):
+        if not self._is_swap_valid(tx):
             Logs(log=f"Validation failed. Signer:\n {self.multisig.signer_acc_name}.\ntx id:{tx.id}.")
             return
 
@@ -61,11 +71,11 @@ class Signer:
         if success:
             Signatures(tx_id=tx.id, signer=self.multisig.signer_acc_name, signed_tx=signed_tx).save()
 
-    def is_signed(self, tx: ETHSwap) -> bool:
+    def _is_swap_signed(self, tx: ETHSwap) -> bool:
         """ Returns True if tx was already signed, else False """
         return Signatures.objects(tx_id=tx.id, signer=self.multisig.signer_acc_name).count() > 0
 
-    def is_valid(self, tx: ETHSwap) -> bool:
+    def _is_swap_valid(self, tx: ETHSwap) -> bool:
         """Assert that the data in the unsigned_tx matches the tx on the chain"""
         log = event_log(tx.tx_hash, 'Swap', self.provider, self.contract.contract)
         unsigned_tx = json.loads(tx.unsigned_tx)
@@ -95,3 +105,38 @@ class Signer:
     def decrypt(unsigned_tx: Dict):
         msg = unsigned_tx['value']['msg'][0]['value']['msg']
         return decrypt(msg)
+
+    def handle_submission(self,  submission_id: int):
+        """Validates submission event with scrt network and sends confirmation if valid"""
+        self._validated_and_confirm(submission_id)
+
+    def _submission_catch_up(self):
+        """Iterates over the 'transactions map' of the smart contract, add validates tx if required"""
+        # iterate the map
+        # for each one, call _approve_and_submit
+
+    def _validated_and_confirm(self, submission_id: int):
+        """Given """
+        data = self._submission_data(submission_id)
+        with self.submissions_lock:
+            if self._is_submission_transactions_approved(data) and self._is_confirmed(submission_id):
+                self._confirm_transaction(data)
+
+            self.processed_submission_tx.add(submission_id)
+
+    def _submission_data(self, transaction_id) -> Dict[str, any]:
+        pass
+
+    def _is_submission_transactions_approved(self, submission_data: Dict[str, any]) -> bool:
+        pass
+
+    def _is_confirmed(self, submission_id: int) -> bool:
+        """Checks with the data on the contract if signer already added confirmation"""
+        pass
+
+    def _confirm_transaction(self, submission_data: Dict[str, any]) -> None:
+        """
+        Sends 'confirmTransaction' tx to the contract.
+        Note: This operation costs gas
+        """
+        pass
