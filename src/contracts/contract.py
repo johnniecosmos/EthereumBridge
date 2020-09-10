0b91771f97b8bfc70a8cf592511cb61d96f7d8f8
Guy <tbk.guy@gmail.com>
2020-09-10 13:01:27 +0300

    update abi generation

--- src/contracts/contract.py
+++ src/contracts/contract.py
@@ -4,19 +4,21 @@ from web3 import Web3
 
 from src.util.web3 import normalize_address
 
+# TODO: move to config?
 abi_path = r"/home/guy/Workspace/dev/EthereumBridge/src/contracts/MultiSigSwapWallet.json"
-
-with open(abi_path, "r") as f:
-    abi = json.load(f)['abi']
 
 
 class Contract:
-    abi = abi
-
+    """Container for contract relevant data"""
     def __init__(self, provider: Web3, contract_address: str):
         self.address = contract_address
-
+        self.abi = self.load_abi(abi_path)
         self.contract = provider.eth.contract(address=self.normalized_address(), abi=self.abi)
 
     def normalized_address(self):
         return normalize_address(self.address)
+
+    @staticmethod
+    def load_abi(abi_path_: str) -> str:
+        with open(abi_path_, "r") as f:
+            return json.load(f)['abi']
