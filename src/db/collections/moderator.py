64b44b942d7b0940d59292523fd61e21ec3a9f1e
Guy <tbk.guy@gmail.com>
2020-09-10 23:22:50 +0300

    todo removed

--- src/db/collections/moderator.py
+++ src/db/collections/moderator.py
@@ -1,6 +1,5 @@
 from mongoengine import Document, IntField
 
 
-# TODO: HALP, stupid name is stupid! Mr. code reviewer - be my hero.
 class ModeratorData(Document):
     last_block = IntField(required=True)
